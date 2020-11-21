6569d1a2aaffb929d788e16b6056fc59ba33d1ce
MWRose <mwra2018@mymail.pomona.edu>
2020-11-20 21:04:41 -0800

    graphing

--- main.py
+++ main.py
@@ -1,24 +1,80 @@
-from subprocess import *
+from typing import Iterable
+from mctsconfig import MCTSConfig
+import subprocess
+import os
+import pandas as pd
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
 
-def simulate(*args) -> int:
-    process = Popen(['java', '-cp', 'STACSettlers-1.0-bin.jar', 'soc.robot.stac.simulation.Simulation']+'../StacSettlers/target/config-simple.txt', stdout=PIPE, stderr=PIPE)
-    ret = []
-    while process.poll() is None:
-        line = process.stdout.readline()
-        if line != '' and line.endswith('\n'):
-            ret.append(line[:-1])
-    stdout, stderr = process.communicate()
-    ret += stdout.split('\n')
-    if stderr != '':
-        ret += stderr.split('\n')
-    ret.remove('')
+configs_directory = "/home/max/Documents/ai/CatanAI/configs/"
+ITERATION_RANGE = 1000
 
-    return ret
 
+def simulate(args) -> int:
+    #run_config()
+    #parse_result()
+    pass
+
+def run_config(config: MCTSConfig):
+    """ Take a MCTSConfig and run the specified simulation """
+    config_file = config.get_config_path()
+
+    # Might cause concurrent errors
+    stream = os.popen("java -cp STACSettlers-1.0-bin.jar soc.robot.stac.simulation.Simulation " + config_file).read()
+ 
+# def parse_result(config: MCTSConfig) -> float:
+#     """ Takes a MCTSConfig and returns its fitness (win rate)"""
+#     file_name = glob.glob('results/'+ config.get_name() + '*' + '/results.txt')[0]
+#     num_games = config.get_num_games()
+#     # Look in results folder for this name
+#     df = pd.read_csv('/home/max/Documents/ai/StacSettlers/target/' + file_name, sep='\t')
+#     total_wins = sum(df['Winner1'])
+#     win_rate = total_wins/num_games
+#     return win_rate
+
+
+# Iterations test
+def test_iterations(iters):
+    mcts_config_iterations = []
+    for iterations in iters:
+        mcts_config = MCTSConfig(iterations, .5, 100000, 1, False, 5)
+        run_config(mcts_config)
+        mcts_config.set_fitness()
+        mcts_config_iterations.append(mcts_config)
+
+    return mcts_config_iterations
+
+# def test_cp(cps):
+#     mcts_configs_cp = []
+#     for cp in cps:
+#         mcts_config = MCTSConfig(2000, cp, 10000, 1, False, 10)
+#         run_config(mcts_config)
+#         mcts_config.set_fitness()
+
+#     return mcts_configs_cp
+    
 
 # call our ga() function here
 def main():
-    simulate('../StacSettlers/target/config-simple.txt')
+    os.chdir("/home/max/Documents/ai/StacSettlers/target")
+    # print(os.getcwd())
+    # simulate('../StacSettlers/target/config-simple.txt')
+    iteration_configs = test_iterations([3000, 5000])
+    fitnesses = []
+    iterations = []
+    for config in iteration_configs:
+        fitnesses.append(config.get_fitness())
+        iterations.append(config.get_iterations())
+    
+    plt.plot(iterations, fitnesses)
+    plt.xlabel("Iterations")
+    plt.ylabel("Win Rate")
+    plt.show()
+
+    # Exploration test
+    # test_cp(range(0, 1, .1))
+
 
 if __name__ == "__main__":
     main()
--- mctsconfig.py
+++ mctsconfig.py
@@ -1,52 +1,70 @@
+from datetime import datetime
+import os
+import pandas as pd
+import glob
+
+configs_directory = "/home/max/Documents/ai/CatanAI/configs/"
 
 class MCTSConfig:
     """Creates an holds an MCTS config"""
 
     def __init__(self, iterations: int, cp: float,
-                 max_tree_size: int, min_visits: int, rave: bool):
+                 max_tree_size: int, min_visits: int, rave: bool, num_games: int):
         self.iterations = iterations
         self.cp = cp
         self.max_tree_size = max_tree_size
         self.min_visits = min_visits
         self.rave = rave
         self.mcts_line = ""
-        self.name = ""
-        self.file_path = ""
-        self.num_games = 1
+        self.name = str(datetime.now().time())
+        self.config_path = configs_directory + self.name
+        self.num_games = num_games
+        self.fitness = 0.0
+        self.create_mcts_line()
+        self.create_config_file()
 
     def create_mcts_line(self):
         """
         Creates the line to be added to the config file
         """
-
         # Configure the line to be added to the config file
-        self.mcts_lineline = "Agent=1,TypedMCTS,mcts,MCTS_ITERATIONS:{iterations}|MCTS_THREADS:4|MCTS_Cp:{cp}|MCTS_TYPED_ROLLOUTS|MCTS_MINVISITS:{min_visits}|MCTS_MAX_TREE_SIZE:{max_tree_size}|MCTS_OFFERS_LIMIT:3".format(
+        self.mcts_line = "Agent=1,TypedMCTS,mcts,MCTS_ITERATIONS:{iterations}|MCTS_THREADS:4|MCTS_Cp:{cp}|MCTS_TYPED_ROLLOUTS|MCTS_MINVISITS:{min_visits}|MCTS_MAX_TREE_SIZE:{max_tree_size}|MCTS_OFFERS_LIMIT:3".format(
             iterations=self.iterations, cp=self.cp, min_visits=self.min_visits, max_tree_size=self.max_tree_size)
         if self.rave:
             self.mcts_line += "|MCTS_UCT_RAVE"
-
-    def create_file_path(self):
-        pass
+            
 
     def create_config_file(self):
 
         # Structure of the config file
+        assert self.mcts_line, "MCTS Line not created"
         lines = [
-            "Games=" + self.num_games,
+            "Games=" + str(self.num_games),
             "Log=false",
             "UseParser=false",
             "ChatNegotiation=true",
             "FullyObservable=true",
+            "PlayerToStart=-1",
             "~",
             self.name,
-            "Agent=3,Stac,stac,TRY_N_BEST_BUILD_PLANS:0|FAVOUR_DEV_CARDS:-5",
-            self.mcts_line
+            self.mcts_line,
+            "Agent=3,Stac,stac,TRY_N_BEST_BUILD_PLANS:0|FAVOUR_DEV_CARDS:-5"
         ]
 
         # If there is a file path write it
-        assert self.file_path, "File path not specified for config {}".format(self.name)
-        with open(self.file_path, "w") as config_file:
-            config_file.writelines(lines)
+        assert self.config_path, "File path not specified for config {}".format(self.name)
+        with open(self.config_path, "w") as config_file:
+            join_lines = "\n".join(lines)
+            config_file.write(join_lines)
+
+    def set_fitness(self):
+        """ Takes a MCTSConfig and returns its fitness (win rate)"""
+        file_name = glob.glob('results/'+ self.get_name() + '*' + '/results.txt')[0]
+        num_games = self.get_num_games()
+        # Look in results folder for this name
+        df = pd.read_csv('/home/max/Documents/ai/StacSettlers/target/' + file_name, sep='\t')
+        total_wins = sum(df['Winner1'])
+        self.fitness = total_wins/num_games
 
     def get_iterations(self):
         return self.iterations
@@ -62,6 +80,18 @@ class MCTSConfig:
 
     def get_rave(self):
         return self.rave
+
+    def get_num_games(self):
+        return self.num_games
+
+    def get_config_path(self):
+        return self.config_path
+
+    def get_name(self):
+        return self.name
+
+    def get_fitness(self):
+        return self.fitness
 
 
 
